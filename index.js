fdb217b9bb2ae5ad7bf69fdb9ba3fcf7ba615738
Víctor Borrego Pérez <vborrego@b2bsalud.com>
2019-03-23 21:39:18 +0100

    Added travis file

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var express = require('express');
+var express = require("express");
 
 const PORT = 8888;
+const MESSAGE = process.env.MESSAGE ? process.env.MESSAGE : "No message";
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.get("/", function(req, res) {
   console.log(req);
-  res.send('Bienvenido');
+  res.send("Hello world! - Message: " + MESSAGE);
 });
 
-app.get('/:name', function (req, res) {
+app.get("/:name", function(req, res) {
   console.log(req);
-  res.send('Hola ' + req.params.name);
+  res.send("Hello " + req.params.name + " - Message: " + MESSAGE);
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log("Running on http://localhost:" + PORT);
